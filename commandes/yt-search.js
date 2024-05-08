d299962cbf2afefe1dbcac06328f5807e351863b
Were Lenox <104990838+lenodewere@users.noreply.github.com>
2024-05-08 21:24:30 +0300

    Lennie

--- commandes/yt-search.js
+++ commandes/yt-search.js
@@ -21,7 +21,7 @@ france({ nomCom: "yts", categorie: "Search", reaction: "✋" }, async (dest, zk,
     for (let i = 0; i < 10; i++) {
       captions += `----------------\nTitle: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
     }
-    captions += "\n======\n*powered by LENNIE-W*";
+    captions += "\n======\n*powered by LEN MD*";
 
     // repondre(captions)
     zk.sendMessage(dest, { image: { url: resultat[0].thumbnail }, caption: captions }, { quoted: ms });
@@ -77,9 +77,7 @@ _*En cours de téléchargement...*_\n\n`
 
     fileStream.on('finish', () => {
       // Envoi du fichier vidéo en utilisant l'URL du fichier local
-      zk.sendMessage(origineMessage, { video: { url: `./${filename}` }, caption: "Powered by *LENNIE-W*", gifPlayback: false }, { quoted: ms });
-
-    });
+      zk.sendMessage(origineMessage, { video: { url: `./${filename}` }, caption: "Powered by *LEN MD*", gifPlayback: false }, { quoted: ms })LEN MD);
 
     fileStream.on('error', (error) => {
       console.error('Erreur lors de l\'écriture du fichier vidéo :', error);
