383791fc4f7c8407dbd9e99979d97a2c09e9a85c
Were Lenox <104990838+lenodewere@users.noreply.github.com>
2024-05-08 18:40:12 +0300

    Lennie

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -57,7 +57,7 @@ let linkkk = `https://api.screenshotmachine.com/?key=c04d3a&url=${encodeURICompo
 
 let res = await getBuffer(linkkk);
 
-await zk.sendMessage(dest, { image: res, caption: 'Web Screenshot by *LENNIE-W*'}, { quoted: ms });
+await zk.sendMessage(dest, { image: res, caption: 'Web Screenshot by *LEN MD*'}, { quoted: ms });
 
 
 }
